bc075a2847e7212ce85a86b4039adaf7b4f43896
김태민 <seolin1218@gmail.com>
2023-03-14 15:50:07 +0900

    [fix] : 스크립트 파일 생성하고 순차실행되게 수정, db 접근 수정

---
+++ docker-script.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+nohup /usr/local/bin/docker stop gascharge-app-batch > nohup/nohup-app-batch-stop.out 2>&1 ;
+nohup /usr/local/bin/docker rm gascharge-app-batch > nohup/nohup-app-batch-rm.out 2>&1 ;
+nohup /usr/local/bin/docker rmi gascharge-app-batch > nohup/nohup-app-batch-rmi.out 2>&1 ;
+nohup /usr/local/bin/docker build -t gascharge-app-batch k8s/gascharge-app-batch/ > nohup/nohup-app-batch-build.out 2>&1 ;
+nohup /usr/local/bin/docker run --name gascharge-app-batch -it -d -p 8400:8400 --privileged --cgroupns=host -v /sys/fs/cgroup:/sys/fs/cgroup:rw gascharge-app-batch /usr/sbin/init > nohup/nohup-app-batch-run.out 2>&1 ;
